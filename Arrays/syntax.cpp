b72791661cca9d23cc3f55afb1b2e8b439997185
Hariom <hariomacharya1@gmail.com>
2026-08-18 22:43:56 +0530

    started arrays basics and fundamentals

---
+++ Arrays/syntax.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[4]; // Declaration
+    
+    for(int i=0;i<=4;i++){
+        cin>>arr[i];
+    }
+    
+    for(int i=0;i<4;i++){
+    cout<<arr[i]<<" ";
+    }
+
+}
--- Recursion/power_recurion.cpp
+++ Recursion/power_recurion.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
 
-int fact(int n){
-    if(n==1 || n==0) return 1;
-    return n*fact(n-1);
+int power(int a,int b){
+    if(b==0) return 1;
+    return a*power(a,b-1);
 }
 
 int main(){
     int a;
-    cout<<"Enter a number: ";
-    cin>>n;
-    cout<<fact(n);
+    cout<<"Enter base: ";
+    cin>>a;
+    int b;
+    cout<<"Enter power: ";
+    cin>>b;
+    cout<<power(a,b);
 }
